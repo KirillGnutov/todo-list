711f020fd67883f117bb78f83abbd8f382a2bb0d
kirill <kirillgnutov2@gmail.com>
2025-08-09 13:58:28 +0300

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import React,{Suspense} from "react";
-import {HashRouter, Routes, Route} from "react-router-dom";
+import React, { Suspense } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Styles from "./App.module.css";
 import HomePage from "./pages/Home/Home";
 
 const rootes = {
-    "/": HomePage
-}
+  "/": HomePage,
+};
 
-const App = ()=>{
-    return(
-        <HashRouter>
-            <Layout>
-                <Suspense fallback = {<div>loading...</div>}>
-                    <Routes>
-                        <Roote path="/" element={<HomePage/>}/>
-                        {Object.entries(rootes).map(([path,Component])=>{
-                            <Route key={path} path={path} element={<Component/>}/>
-                        })}
-                    </Routes>
-                </Suspense>
-            </Layout>
-        </HashRouter>
-    )
-}
-export default App
+const App = () => {
+  return (
+    <HashRouter>
+      <Layout>
+        <Suspense fallback={<div>loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {Object.entries(rootes).map(([path, Component]) => {
+              return <Route key={path} path={path} element={<Component />} />;
+            })}
+          </Routes>
+        </Suspense>
+      </Layout>
+    </HashRouter>
+  );
+};
+
+export default App;
